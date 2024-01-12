76dd62b98c9e4a906851bae71ad305524f71192b
Leeyoujin00 <cymn6032@gmail.com>
2024-01-12 21:15:07 +0900

    Create initial settings

---
+++ demo/src/main/java/gdsc/demo/LustleApplication.java
@@ -0,0 +1,13 @@
+package gdsc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LustleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LustleApplication.class, args);
+	}
+
+}
